025162dcf633250298140dca907f79347d3f50aa
Tushar Prasad <prasadtushar29@gmail.com>
2022-03-10 15:17:02 +0530

    Initial Setups

---
+++ src/pages/home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+export default function Home() {
+
+
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+// npm install firebase
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+//
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD5CqzwW07RCrYpG8xi4ipqowxn6h7bXdQ",
+//   authDomain: "colon-51fe9.firebaseapp.com",
+//   projectId: "colon-51fe9",
+//   storageBucket: "colon-51fe9.appspot.com",
+//   messagingSenderId: "137962430904",
+//   appId: "1:137962430904:web:b56c1ee8e0fff2d7b46f4d",
+//   measurementId: "G-DQ0M55TWPG"
+// };
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
